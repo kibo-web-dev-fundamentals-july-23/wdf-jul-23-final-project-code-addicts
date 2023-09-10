2ee85f0d2cfc0915926ea868865795cc243dc96b
samzyconcepts <samson.oderinwale@kibo.school>
2023-09-10 22:16:32 +0100

    correction on the date calculation logic

--- script.js
+++ script.js
@@ -16,113 +16,126 @@
  *      Get the day of month, from 1 to 31, the name of the method does look a little bit strange.
  */
 
-const calculateBtn = document.querySelector('button'),
-	inputContainer = document.querySelector('.input-container')
+const calculateBtn = document.querySelector("button"),
+    inputContainer = document.querySelector(".input-container");
 
-calculateBtn.addEventListener('click', () => {
-	const dayInput = document.querySelector('#day').value,
-		monthInput = document.querySelector('#month').value,
-		yearInput = document.querySelector('#year').value,
-		dayOutput = document.querySelector('.result-container .day-value'),
-		monthOutput = document.querySelector('.result-container .month-value'),
-		yearOutput = document.querySelector('.result-container .year-value')
+calculateBtn.addEventListener("click", () => {
+    const dayInput = document.querySelector("#day").value,
+        monthInput = document.querySelector("#month").value,
+        yearInput = document.querySelector("#year").value,
+        dayOutput = document.querySelector(".result-container .day-value"),
+        monthOutput = document.querySelector(".result-container .month-value"),
+        yearOutput = document.querySelector(".result-container .year-value");
 
-	if (dayInput === '' || monthInput === '' || yearInput === '') {
-		labels = document.getElementsByTagName('label')
-		inputContainer.classList.add('error')
+    if (dayInput === "" || monthInput === "" || yearInput === "") {
+        labels = document.getElementsByTagName("label");
+        inputContainer.classList.add("error");
 
-		Array.from(labels).forEach((label) => {
-			const errorMsg = createText('This field is required')
-			label.appendChild(errorMsg)
+        Array.from(labels).forEach((label) => {
+            const errorMsg = createText("This field is required");
+            label.appendChild(errorMsg);
 
-			// remove the error after 3seconds
-			setTimeout(() => {
-				inputContainer.classList.remove('error')
-				label.removeChild(errorMsg)
-			}, 3000)
-		})
-	}
+            // remove the error after 3seconds
+            setTimeout(() => {
+                inputContainer.classList.remove("error");
+                label.removeChild(errorMsg);
+            }, 3000);
+        });
+    }
 
-	if (monthInput > 12) {
-		labels = document.getElementsByTagName('label')
+    if (monthInput > 12) {
+        labels = document.getElementsByTagName("label");
 
-		inputContainer.classList.add('error')
-		const errorMsg = createText('Must be a valid month')
+        inputContainer.classList.add("error");
+        const errorMsg = createText("Must be a valid month");
 
-		labels[1].appendChild(errorMsg)
+        labels[1].appendChild(errorMsg);
 
-		setTimeout(() => {
-			inputContainer.classList.remove('error')
-			labels[1].removeChild(errorMsg)
-		}, 3000)
-	}
+        setTimeout(() => {
+            inputContainer.classList.remove("error");
+            labels[1].removeChild(errorMsg);
+        }, 3000);
+    }
 
-	const currentDate = new Date(),
-		currentYear = currentDate.getFullYear(),
-		currentMonth = currentDate.getMonth() + 1,
-		currentDay = currentDate.getDate()
+    const currentDate = new Date(),
+        currentYear = currentDate.getFullYear(),
+        currentMonth = currentDate.getMonth() + 1,
+        currentDay = currentDate.getDate();
 
-	if (yearInput > currentYear) {
-		labels = document.getElementsByTagName('label')
+    if (yearInput > currentYear) {
+        labels = document.getElementsByTagName("label");
 
-		inputContainer.classList.add('error')
+        inputContainer.classList.add("error");
 
-		const errorMsg = createText('Must be in the past')
+        const errorMsg = createText("Must be in the past");
 
-		labels[2].appendChild(errorMsg)
-		setTimeout(() => {
-			inputContainer.classList.remove('error')
-			labels[2].removeChild(errorMsg)
-		}, 3000)
-	}
+        labels[2].appendChild(errorMsg);
+        setTimeout(() => {
+            inputContainer.classList.remove("error");
+            labels[2].removeChild(errorMsg);
+        }, 3000);
+    }
 
-	let ageDay = currentDay - dayInput,
-		ageMonth = currentMonth - monthInput,
-		ageYear = currentYear - yearInput
+    const lastDayOfCurrentMonth = new Date(
+        currentYear,
+        currentMonth - 1,
+        0
+    ).getDate();
 
-	const lastDayOfMonth = new Date(currentYear, currentMonth - 1, 0).getDate()
+    const lastDayOfUserInputMonth = new Date(
+        yearInput,
+        monthInput,
+        0
+    ).getDate();
 
-	if (dayInput > 31 || dayInput > lastDayOfMonth) {
-		labels = document.getElementsByTagName('label')
+    if (dayInput > lastDayOfUserInputMonth) {
+        labels = document.getElementsByTagName("label");
 
-		inputContainer.classList.add('error')
+        inputContainer.classList.add("error");
 
-		const errorMsg = createText('Must be a valid day')
+        const errorMsg = createText("Must be a valid day");
 
-		labels[0].appendChild(errorMsg)
+        labels[0].appendChild(errorMsg);
 
-		setTimeout(() => {
-			inputContainer.classList.remove('error')
-			labels[0].removeChild(errorMsg)
-		}, 3000)
-	}
+        setTimeout(() => {
+            inputContainer.classList.remove("error");
+            labels[0].removeChild(errorMsg);
+        }, 3000);
+    }
 
-	if (ageDay < 0) {
-		ageMonth--
-		ageDay += lastDayOfMonth
-	}
+    let ageDay = currentDay - dayInput,
+        ageMonth = currentMonth - monthInput,
+        ageYear = currentYear - yearInput;
 
-	if (ageMonth < 0 || (ageMonth === 0 && ageDay < 0)) {
-		ageYear--
-		ageMonth += 12
-	}
+    if (ageDay < 0) {
+        ageMonth--;
+        ageDay += lastDayOfCurrentMonth;
+    }
 
-	if (
-		(dayInput !== '' && dayInput < 31) &&
-		(monthInput !== '' && monthInput <= 12) &&
-		(yearInput !== '' && yearInput < currentDate)
-	) {
-		dayOutput.innerHTML = ageDay
-		monthOutput.innerHTML = ageMonth
-		yearOutput.innerHTML = ageYear
-	} 
-})
+    if (ageMonth < 0 || (ageMonth === 0 && ageDay < 0)) {
+        ageYear--;
+        ageMonth += 12;
+    }
+
+    if (
+        dayInput !== "" &&
+        dayInput <= lastDayOfUserInputMonth &&
+        monthInput !== "" &&
+        monthInput <= 12 &&
+        yearInput !== "" &&
+        yearInput <= currentYear
+    ) {
+        dayOutput.innerHTML = ageDay;
+        monthOutput.innerHTML = ageMonth;
+        yearOutput.innerHTML = ageYear;
+    }
+});
 
 // function to create a the error message in a paragraph
 function createText(text) {
-	const errorMsg = document.createElement('p')
-	errorMsg.classList.add('error')
-	errorMsg.textContent = `${text}`
+    const errorMsg = document.createElement("p");
+    errorMsg.classList.add("error");
+    errorMsg.textContent = `${text}`;
 
-	return errorMsg
+    return errorMsg;
 }
